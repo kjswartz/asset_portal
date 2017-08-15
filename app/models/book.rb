a20ebd8c10327329033fd4f363eb97a533c5bab2
Kyle Swartz <kyle.swartz.3@gmail.com>
2017-08-14 22:22:25 -0400

    update git ignore and work through neo4j sample ruby project

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,18 @@ class Book < Asset
   property :year_published, type: Integer
 
   has_one :in, :author, type: :CREATED, model_class: :User
-  has_one :out, :category, type: :HAS_CATEGORY
+  has_many :out, :categories, type: :HAS_CATEGORY
+
+  def self.recommendations
+    recommendations = all(:book).
+                        categories(:category).
+                        books(:other_book).
+                        where('book <> other_book').
+                        query.
+                        with('book, other_book, count(category) AS count').
+                        where('count > 1').
+                        pluck('book.isbn', 'collect(other_book)')
+
+    Hash[*recommendations.flatten(1)]
+  end
 end
